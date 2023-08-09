c24658734bc8642332926a5adf3af90f78a2aab9
tan-sd <shengdatan@gmail.com>
2023-08-10 01:56:30 +0800

    GSAP Animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,44 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import { gsap } from "gsap";
 
-export const Footer = () => {
-    const secondaryRef = useRef(null);
+export const Footer = ({ isMenuVisible }) => {
+    const footerWrapRef = useRef();
+    const footerRef = useRef(null);
+
+    useEffect(() => {
+        if (isMenuVisible) {
+            footerWrapRef.current.classList.add("navbar-close");
+            gsap.timeline({ defaults: { duration: 0.7, ease: "power2" } }).to(
+                [footerRef.current],
+                {
+                    y: "-150%",
+                    rotate: -5,
+                }
+            );
+        } else {
+            footerWrapRef.current.classList.remove("navbar-close");
+            gsap.timeline({ defaults: { duration: 1.2, ease: "expo" } })
+                .set(footerRef.current, {
+                    y: "150%",
+                    rotate: 15,
+                })
+                .to(footerRef.current, {
+                    y: "0%",
+                    rotate: 0,
+                });
+        }
+    }, [isMenuVisible]);
 
     return (
-        <div className="footer oh">
-            <span className="oh__inner" ref={secondaryRef}>
+        <div className="footer oh" ref={footerWrapRef}>
+            <span className="oh__inner" ref={footerRef}>
                 © 2023 Tan Sheng Da
             </span>
         </div>
     );
+};
+
+Footer.propTypes = {
+    isMenuVisible: PropTypes.bool.isRequired,
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,71 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import { gsap } from "gsap";
 import logo from "../assets/image/logo/logo.png";
 
-export const Navbar = () => {
+export const Navbar = ({ isMenuVisible, onMenuButtonClick }) => {
+    const navbarRef = useRef();
     const menuOpenCtrlRef = useRef(null);
     const aboutOpenCtrlRef = useRef(null);
     const headingRef = useRef(null);
+
+    useEffect(() => {
+        if (isMenuVisible) {
+            navbarRef.current.classList.add("navbar-close");
+            gsap.timeline({ defaults: { duration: 0.7, ease: "power2" } }).to(
+                [
+                    menuOpenCtrlRef.current,
+                    aboutOpenCtrlRef.current,
+                    headingRef.current,
+                ],
+                {
+                    y: "-150%",
+                    rotate: -5,
+                }
+            );
+        } else {
+            navbarRef.current.classList.remove("navbar-close");
+            gsap.timeline({ defaults: { duration: 1.2, ease: "expo" } })
+                .set(
+                    [
+                        menuOpenCtrlRef.current,
+                        aboutOpenCtrlRef.current,
+                        headingRef.current,
+                    ],
+                    {
+                        y: "150%",
+                        rotate: 15,
+                    }
+                )
+                .to(
+                    [
+                        menuOpenCtrlRef.current,
+                        aboutOpenCtrlRef.current,
+                        headingRef.current,
+                    ],
+                    {
+                        y: "0%",
+                        rotate: 0,
+                    }
+                );
+        }
+    }, [isMenuVisible]);
+
     return (
         <>
-            <div className="navbar-container">
-                <div>
+            <div className="navbar-container" ref={navbarRef}>
+                <div className="oh">
                     <span className="oh__inner" ref={headingRef}>
                         <img src={logo} alt="logo" className="logo" />
                     </span>
                 </div>
                 <div className="navbar-link-container">
                     <button className="menu-link oh unbutton">
-                        <span className="oh__inner" ref={menuOpenCtrlRef}>
+                        <span
+                            className="oh__inner"
+                            ref={menuOpenCtrlRef}
+                            onClick={onMenuButtonClick}
+                        >
                             Projects
                         </span>
                     </button>
@@ -28,4 +78,9 @@ export const Navbar = () => {
             </div>
         </>
     );
+};
+
+Navbar.propTypes = {
+    isMenuVisible: PropTypes.bool.isRequired,
+    onMenuButtonClick: PropTypes.func.isRequired,
 };
