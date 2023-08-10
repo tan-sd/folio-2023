a82af44ac373ff5918b76e5ba1b1b4d804d26afd
tan-sd <shengdatan@gmail.com>
2023-08-10 18:18:39 +0800

    Add About Components and Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Menu } from "./components/Menu";
+import { About } from "./components/About";
 import { Footer } from "./components/Footer";
 
 function App() {
     const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [isAboutVisible, setIsAboutVisible] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuVisible(!isMenuVisible);
+    };
+
+    const toggleAbout = () => {
+        setIsAboutVisible(!isAboutVisible);
     };
 
     return (
@@ -15,10 +21,16 @@ function App() {
             <Navbar
                 isMenuVisible={isMenuVisible}
                 onMenuButtonClick={toggleMenu}
+                isAboutVisible={isAboutVisible}
+                onAboutButtonClick={toggleAbout}
             />
             <Menu
                 isMenuVisible={isMenuVisible}
                 onMenuButtonClick={toggleMenu}
+            />
+            <About
+                isAboutVisible={isAboutVisible}
+                onAboutButtonClick={toggleAbout}
             />
             <Footer isMenuVisible={isMenuVisible} />
         </>
