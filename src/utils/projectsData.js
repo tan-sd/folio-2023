ca9af9473e4562b91e89642517a8a224ec72730e
tan-sd <shengdatan@gmail.com>
2023-08-13 23:47:28 +0800

    Initalise Project Data

---
+++ src/utils/projectsData.js
@@ -0,0 +1,170 @@
+export const projectsData = [
+    {
+        name: "PokéGen",
+        text: "My first ever web application that uses REST to retrieve random Pokémon names, IDs, and images sourced from the PokéAPI.",
+        tools: [
+            {
+                name: "HTML",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+            },
+            {
+                name: "CSS",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+            },
+            {
+                name: "JavaScript",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+            },
+        ],
+        links: [
+            {
+                name: "Website",
+                link: "https://tan-sd.github.io/pokegen/",
+            },
+            {
+                name: "Github",
+                link: "https://github.com/tan-sd/pokemon-random-generator",
+            },
+        ],
+    },
+    {
+        name: "BOJIO",
+        text: "A vibrant social platform for Singaporeans to connect, discover events, and cultivate relationships. Users can join and organize 'jios' based on personal interests, ensuring a fulfilling social experience.",
+        tools: [
+            {
+                name: "Vue",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+            },
+            {
+                name: "Bootstrap",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+            },
+            {
+                name: "ThreeJS",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/threejs/threejs-original.svg",
+            },
+            {
+                name: "Firebase",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+            },
+        ],
+        links: [
+            {
+                name: "Website",
+                link: "https://bojio.netlify.app/",
+            },
+            {
+                name: "Github",
+                link: "https://github.com/tan-sd/bojio",
+            },
+        ],
+    },
+    {
+        name: "MERNfolio",
+        text: "A capstone project from SMU .Hack MERN Stack Series, leveraging the immense potential of the MERN stack to develop a comprehensive web application.",
+        tools: [
+            {
+                name: "MongoDB",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+            },
+            {
+                name: "React",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+            },
+            {
+                name: "Express",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+            },
+            {
+                name: "NodeJS",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg",
+            },
+            {
+                name: "Bootstrap",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+            },
+            {
+                name: "CSS",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+            },
+        ],
+        links: [
+            {
+                name: "Website",
+                link: "https://tan-sd.github.io/mern-portfolio/",
+            },
+            {
+                name: "Github",
+                link: "https://github.com/tan-sd/mern-portfolio",
+            },
+        ],
+    },
+    {
+        name: "MakanBoleh",
+        text: "This microservice application promotes community sharing of free food to combat food wastage. Users can share and explore available offerings.",
+        tools: [
+            {
+                name: "Vue",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+            },
+            {
+                name: "Bootstrap",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+            },
+            {
+                name: "Firebase",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+            },
+            {
+                name: "Docker",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+            },
+            {
+                name: "Flask",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg",
+            },
+        ],
+        links: [
+            {
+                name: "YouTube",
+                link: "https://www.youtube.com/watch?v=FqJ4LMRt8E4",
+            },
+            {
+                name: "Github",
+                link: "https://github.com/tan-sd/makan-boleh",
+            },
+        ],
+    },
+    {
+        name: "Tasktopia",
+        text: "A mobile application that gamifies employee engagement and productivity. Nurture virtual pets, complete tasks, and claim rewards.",
+        tools: [
+            {
+                name: "React",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+            },
+            {
+                name: "Expo",
+                img: "https://cdn.cdnlogo.com/logos/e/72/expo-go-app.svg",
+            },
+            {
+                name: "ThreeJS",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/threejs/threejs-original.svg",
+            },
+            {
+                name: "Firebase",
+                img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+            },
+        ],
+        links: [
+            {
+                name: "YouTube",
+                link: "https://www.youtube.com/watch?v=RlHZ63VFuDM",
+            },
+            {
+                name: "Github",
+                link: "https://github.com/tan-sd/tasktopia",
+            },
+        ],
+    },
+];
