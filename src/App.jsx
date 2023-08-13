8ebe5efc5a7170c6af0d7b2b4bb9fe53f531501c
tan-sd <shengdatan@gmail.com>
2023-08-13 23:47:58 +0800

    Add Components and Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Menu } from "./components/Menu";
+import { Project } from "./components/Project";
 import { About } from "./components/About";
 import { Footer } from "./components/Footer";
+import { CloseProject } from "./components/closeProject";
+
+import { projectsData } from "./utils/projectsData";
 
 function App() {
+    const [isNavVisible, setIsNavVisible] = useState(true);
     const [isMenuVisible, setIsMenuVisible] = useState(false);
     const [isAboutVisible, setIsAboutVisible] = useState(false);
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
 
     const toggleMenu = () => {
         setIsMenuVisible(!isMenuVisible);
@@ -16,25 +26,50 @@ function App() {
         setIsAboutVisible(!isAboutVisible);
     };
 
+    const toggleProject = (projectName) => {
+        setSelectedProject(projectName);
+    };
+
     return (
         <>
             <Navbar
+                isNavVisible={isNavVisible}
+                onNavButtonClick={toggleNav}
                 isMenuVisible={isMenuVisible}
                 onMenuButtonClick={toggleMenu}
                 isAboutVisible={isAboutVisible}
                 onAboutButtonClick={toggleAbout}
             />
             <Menu
+                onNavButtonClick={toggleNav}
                 isMenuVisible={isMenuVisible}
                 onMenuButtonClick={toggleMenu}
+                projectsData={projectsData}
+                selectedProject={selectedProject}
+                onProjectButtonClick={toggleProject}
             />
+            {projectsData.map((project, index) => (
+                <Project
+                    key={index}
+                    projectName={project.name}
+                    projectText={project.text}
+                    projectTools={project.tools}
+                    projectLinks={project.links}
+                    selectedProject={selectedProject}
+                />
+            ))}
             <About
+                onNavButtonClick={toggleNav}
                 isAboutVisible={isAboutVisible}
                 onAboutButtonClick={toggleAbout}
             />
             <Footer
-                isMenuVisible={isMenuVisible}
-                isAboutVisible={isAboutVisible}
+                isNavVisible={isNavVisible}
+            />
+            <CloseProject
+                selectedProject={selectedProject}
+                onProjectButtonClick={toggleProject}
+                onMenuButtonClick={toggleMenu}
             />
         </>
     );
