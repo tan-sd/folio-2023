2a07ddfa254f57da1821c7d9b2f8f01690da1d45
tan-sd <shengdatan@gmail.com>
2023-08-16 22:21:55 +0800

    Configure Model Placement

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, Suspense } from "react";
 import SplitType from "split-type";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera, PresentationControls } from "@react-three/drei";
 
 export const Project = ({
     projectName,
@@ -9,6 +11,7 @@ export const Project = ({
     projectTools,
     projectLinks,
     projectPanel,
+    projectModel,
     selectedProject,
 }) => {
     const projectContentRef = useRef([]);
@@ -158,65 +161,86 @@ export const Project = ({
                     style={{ height: projectPanel[0].size, width: "auto" }}
                 />
             </div>
-            <div className="project__content">
-                <h3 className="project__content-title oh">
-                    <span
-                        className="oh__inner"
-                        ref={(el) => (projectContentRef.current[0] = el)}
-                    >
-                        {projectName}
-                    </span>
-                </h3>
-                <p className="project__content-text" ref={projectTextRef}>
-                    {projectText}
-                </p>
-                <h3 className="project__content-tools-title oh">
-                    <span
-                        className="oh__inner"
-                        ref={(el) => (projectContentRef.current[1] = el)}
-                    >
-                        Tools used:
-                    </span>
-                </h3>
-                <ul className="project__content-tools">
-                    {projectTools.map((tool, index) => (
-                        <li className="oh" key={index}>
+            <div className="project__content-container">
+                {/* <div className="project__canvas"> */}
+                    {/* <Canvas>
+                        <ambientLight color={"#ffffff"} intensity={1.7} />
+                        <pointLight
+                            position={[-0.5, -0.5, 2.5]}
+                            intensity={2}
+                        />
+                        <Suspense fallback={null}>
+                            <PresentationControls snap={true}>
+                                <PerspectiveCamera
+                                    rotation={[0, -2, 0]}
+                                    position={[0.5, -1.35, 1.5]}
+                                >
+                                    <projectModel.model visible={false}/>
+                                </PerspectiveCamera>
+                            </PresentationControls>
+                        </Suspense>
+                    </Canvas> */}
+                {/* </div> */}
+                <div className="project__content">
+                    <h3 className="project__content-title oh">
+                        <span
+                            className="oh__inner"
+                            ref={(el) => (projectContentRef.current[0] = el)}
+                        >
+                            {projectName}
+                        </span>
+                    </h3>
+                    <p className="project__content-text" ref={projectTextRef}>
+                        {projectText}
+                    </p>
+                    <h3 className="project__content-tools-title oh">
+                        <span
+                            className="oh__inner"
+                            ref={(el) => (projectContentRef.current[1] = el)}
+                        >
+                            Tools used:
+                        </span>
+                    </h3>
+                    <ul className="project__content-tools">
+                        {projectTools.map((tool, index) => (
+                            <li className="oh" key={index}>
+                                <div
+                                    className="oh__inner"
+                                    ref={(el) =>
+                                        (projectContentRef.current[index + 2] =
+                                            el)
+                                    }
+                                >
+                                    <img src={tool.img} width="25px" />
+                                    <span className="project__content-tools-name">
+                                        {tool.name}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
+                        <li></li>
+                    </ul>
+                    {projectLinks.map((link, index) => (
+                        <a
+                            className="project__content-link oh"
+                            href={link.link}
+                            target="_blank"
+                            key={index}
+                            rel="noreferrer"
+                        >
                             <div
                                 className="oh__inner"
                                 ref={(el) =>
-                                    (projectContentRef.current[index + 2] = el)
+                                    (projectContentRef.current[
+                                        projectContentRef.current.length + index
+                                    ] = el)
                                 }
                             >
-                                <img
-                                    src={tool.img}
-                                    width="25px"
-                                />
-                                {tool.name}
+                                {link.name}
                             </div>
-                        </li>
+                        </a>
                     ))}
-                    <li></li>
-                </ul>
-                {projectLinks.map((link, index) => (
-                    <a
-                        className="project__content-link oh"
-                        href={link.link}
-                        target="_blank"
-                        key={index}
-                        rel="noreferrer"
-                    >
-                        <div
-                            className="oh__inner"
-                            ref={(el) =>
-                                (projectContentRef.current[
-                                    projectContentRef.current.length + index
-                                ] = el)
-                            }
-                        >
-                            {link.name}
-                        </div>
-                    </a>
-                ))}
+                </div>
             </div>
         </div>
     );
