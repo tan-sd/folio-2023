b014394858acc1a2032ebefacaccd27faf8577b6
tan-sd <shengdatan@gmail.com>
2023-08-17 14:40:00 +0800

    Minor Edits

--- src/experience/Experience.jsx
+++ src/experience/Experience.jsx
@@ -1,8 +1,12 @@
-import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera, PresentationControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+    PerspectiveCamera,
+    PresentationControls,
+    useGLTF,
+} from "@react-three/drei";
 import { Suspense, useEffect, useRef, useState } from "react";
 
-import { Pokegen } from "../assets/model/pokegen/Pokegen";
+import { PokeGen } from "../assets/model/pokegen/Pokegen";
 import { Tasktopia } from "../assets/model/tasktopia/Tasktopia";
 
 export const Experience = ({ selectedProject }) => {
@@ -32,25 +36,16 @@ export const Experience = ({ selectedProject }) => {
     return (
         <div className="project__canvas" ref={projectCanvasRef}>
             <Canvas>
-                <ambientLight color={"#ffffff"} intensity={1.7} />
-                <pointLight position={[-0.5, -0.5, 2.5]} intensity={2} />
+                <ambientLight color={"#ffffff"} intensity={1} />
+                <pointLight position={[-1, -5, 10]} intensity={150} />
                 <Suspense fallback={null}>
                     <PresentationControls snap={true}>
-                        <PerspectiveCamera
-                            rotation={[0, -2.2, 0]}
-                            position={[0.5, 0, 0]}
-                        >
-                            <Pokegen
-                                visible={projectVisibility.pokegen}
-                                scale={1.3}
-                            />
-                        </PerspectiveCamera>
-                        <PerspectiveCamera
-                            rotation={[0, -0.5, 0]}
-                            position={[0.5, 0, 0]}
-                        >
-                            <Tasktopia visible={projectVisibility.tasktopia} scale={1.3}/>
-                        </PerspectiveCamera>
+                        <PokeGen visibleStatus={projectVisibility.pokegen} />
+                    </PresentationControls>
+                    <PresentationControls snap={true}>
+                        <Tasktopia
+                            visibleStatus={projectVisibility.tasktopia}
+                        />
                     </PresentationControls>
                 </Suspense>
             </Canvas>
