070ba9c3545c4d515def6e004ad307670cb7bb1a
tan-sd <shengdatan@gmail.com>
2023-08-22 14:44:03 +0800

    Handle Display Property

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,12 +1,12 @@
 import { Html } from "@react-three/drei";
 
-export const Screen = () => {
+export const Screen = ({isMenuVisible, isAboutVisible, selectedProject}) => {
+    const displayStatus = {
+        display: !isMenuVisible && !isAboutVisible && !selectedProject ? "block" : "none"
+    };
+
     return (
-        <Html
-            className="screen"
-            position={[0, 1.5, -10]}
-            transform
-        >
+        <Html className="screen" position={[0, 1.5, -10]} transform style={{ ...displayStatus}}>
             <iframe src="https://my-room-in-3d-tan-sd.vercel.app/"></iframe>
         </Html>
     );
